b8207b8262388da5f3a3bbe70190e301ea643357
encarju <justine.encarnacion@oocl.com>
2020-11-27 11:03:11 +0800

    Exercise 2

---
+++ Exercise2.py
@@ -0,0 +1,16 @@
+import random
+
+def guessNumber():
+
+    randNum = random.randint(0, 9)
+    guess = False;
+
+    while (not guess):
+        userInput = int(input("Enter a Number"))
+        if(userInput == randNum):
+            print("Exactly Right")
+            guess = True
+        elif(userInput > randNum):
+            print("Too High")
+        else:
+            print("Too Low")
