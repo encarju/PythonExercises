b7d986aa3cae1a7e4f1d695aa1773f2d4e74bed7
encarju <justine.encarnacion@oocl.com>
2020-11-27 15:53:37 +0800

    [Exercise5][ENCARJU] feature: add logic add item and show menu

---
+++ Exercise5.py
@@ -0,0 +1,32 @@
+menu = {}
+
+
+def add_item_to_menu():
+    x = 0
+    while x < 5:
+        item_input = input("Add an Item to Menu: ")
+        price_input = float(input("Set an Item Price: "))
+        menu[item_input] = price_input;
+        x = x + 1
+        choice = input("Want to add more? y/n \n")
+        if choice == "n":
+            break
+
+
+def show_menu():
+    print(menu)
+
+
+if __name__ == "__main__":
+    choice = ""
+    while choice != "x":
+        choice = input("Options\n"
+                       "Choose a to Add Item:\n"
+                       "Choose b to show Menu:\n"
+                       "Choose x to Exit:\n")
+        if choice == "a":
+            add_item_to_menu()
+        elif choice == "b":
+            show_menu()
+        elif choice == "x":
+            exit()
