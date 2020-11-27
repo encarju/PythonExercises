0965b01d7f77283657ee51642e4b63390a26c928
encarju <justine.encarnacion@oocl.com>
2020-11-27 15:01:56 +0800

    [Exercise4][ENCARJU] feature: update logic and created list c

--- Exercise4.py
+++ Exercise4.py
@@ -1,9 +1,10 @@
 list_a = [12, 13, 14, 15, 16, 17, 18, 19]
 list_b = [98, 97, 96, 95, 94, 93, 92, 91]
 
-list_a_odds = list(filter(lambda odd: (odd % 2 != 0), list_a))
-list_b_evens = list(filter(lambda even: (even % 2 != 0), list_b))
-
 if __name__ == "__main__":
-    print(list_a_odds)
-    print(list_b_evens)
+    list_a_odds = list(filter(lambda odd: (odd % 2 != 0), list_a))
+    list_b_evens = list(filter(lambda even: (even % 2 != 0), list_b))
+    list_c = list_a_odds + list_b_evens;
+    print(list_a)
+    print(list_b)
+    print(list_c)
